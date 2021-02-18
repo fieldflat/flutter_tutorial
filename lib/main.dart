ad8c9c1019d0e4cc22d725a8035cd47e94fe175d
Tomonori HIRATA <hirata@sqlab.jp>
2021-02-18 13:21:35 +0900

    画面遷移の例1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(
-    MaterialApp(
-      home: Center(
-        child: Text('Hello World!')
-      ),
-    ),
+    MaterialApp(home: FirstRoute())
   );
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: (){
+                Navigator.push(context,
+                  MaterialPageRoute(
+                    builder: (context) => SecondRoute()
+                  )
+                );
+              },
+              child: Text(
+                'Push Here',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Route'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: Text('Push Here2',
+                style: TextStyle(fontSize: 20),
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
